3f0b94e73c69dda9aa1fdef062e0b1d3802eda99
Frieren0205 <31790618+Frieren0205@users.noreply.github.com>
2023-03-13 02:06:23 +0900

    캐릭터 테스트용 움직임 적용[2023_03_13]

--- Assets/Scripts/Player_Controll.cs
+++ Assets/Scripts/Player_Controll.cs
@@ -5,19 +5,59 @@ using UnityEngine.InputSystem;
 
 public class Player_Controll : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Vector3 MoveDirection;
+    [SerializeField]
+    private float MoveSpeed;
+    public float JumpPower;
+    public Rigidbody rb;
+
+    bool isGrounded;
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool hascontrol = (MoveDirection != Vector3.zero);
+
+        if(hascontrol)
+        {
+            transform.Translate(new Vector3(MoveDirection.x,0,MoveDirection.z) * MoveSpeed * Time.deltaTime);
+            OnFlip();
+            OnJump(MoveDirection.y);
+        }
     }
-    public void PlayerMovement()
+    public void OnFlip()
     {
-        
+        bool isflip = MoveDirection.x > 0; // 좌우 반전을 위해
+        bool flipStay = MoveDirection.x == 0; // 좌우 반전을 유지하기 위해
+        if(isflip && !flipStay)
+        {
+            transform.localScale = new Vector3(-3,3,3);
+        }
+        else if(!isflip && !flipStay) transform.localScale = new Vector3(3,3,3);
+    }
+    public void OnMove(InputAction.CallbackContext value)
+    {
+        Vector3 input = value.ReadValue<Vector3>();
+        if(input != null)
+        {
+            MoveDirection = new Vector3(input.x,input.y,input.z);
+        }
+    }
+    public void OnJump(float value)
+    {
+        if(value == 1 && isGrounded) 
+        {
+            rb.AddForce(Vector3.up * JumpPower,ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+    private void OnCollisionEnter(Collision other) 
+    {
+        isGrounded = true;
     }
 }
