e1a937757728920890a35a2c81e97fee09fedd00
Frieren0205 <31790618+Frieren0205@users.noreply.github.com>
2023-03-25 12:17:38 +0900

    유니티 버전 업그레이드 이후 테스트
    디버깅 오류로 2022.2.12f1로 업그레이드

--- Assets/Scripts/Enemy_Test2.cs
+++ Assets/Scripts/Enemy_Test2.cs
@@ -21,7 +21,7 @@ public class Enemy_Test2 : MonoBehaviour
     {
         path = new NavMeshPath();
         sight = this.gameObject.GetComponentInChildren<EnemySight>();
-        //target_Transform = gameObject.GetComponent<Player_Controll>().transform;
+        target_Transform = FindObjectOfType<Player_Controll>().transform;
     }
 
     void FixedUpdate()
@@ -68,9 +68,9 @@ public class Enemy_Test2 : MonoBehaviour
        }
        if(WayPoints != null)
        {
-            float TonextCorner = Vector3.Distance(transform.position, WayPoints[currentWayPoints]);
+            //float TonextCorner = Vector3.Distance(transform.position, WayPoints[currentWayPoints]);
 
-                currentWayPoints++;
+            //currentWayPoints++;
 
        }
        for(int i = 0; i < path.corners.Length -1; i++)
