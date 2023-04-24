badee7959e2b4b3e666d2d27c5c17abf9cedfbaa
RiHen <31790618+Frieren0205@users.noreply.github.com>
2023-04-25 03:27:17 +0900

    발전기 작업 ing....[2023_04_25]

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Generator : MonoBehaviour
 {
 
     public int GeneratorHP; // 초기 밸런싱 15
     private BoxCollider col;
+
+    private DOTweenAnimation doanimation;
+    private SpriteRenderer sprender;
+    private bool isHit = false;
     public bool isEmergency = false;
     // Start is called before the first frame update
     private void OnEnable() 
     {
         col = this.gameObject.GetComponent<BoxCollider>();
+        sprender = this.gameObject.GetComponentInChildren<SpriteRenderer>();
+        doanimation = this.gameObject.GetComponentInChildren<DOTweenAnimation>();
     }
     // Update is called once per frame
     void Update()
@@ -28,23 +35,33 @@ public class Generator : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other) 
     {
-        if(other.gameObject.tag == "PlayerAttack")
+        if(other.gameObject.tag == "PlayerAttack" && !isHit)
         {
             Debug.Log("피격 인식");
             GeneratorHP -= 1;
+            OnHit();
         }
     }
     
     public void OnHit()
     {
-
+        StartCoroutine(Hitable());
     }
     // 히트 시 무적시간
+    IEnumerator Hitable()
+    {
+        isHit = true;
+        sprender.color = new Color(0.4f,0.4f,0.4f,1);
+        doanimation.DORestart();
+        yield return new WaitForSecondsRealtime(1.25f);
+        isHit = false;
+        sprender.color = new Color(1,1,1,1);
+    }
 
     IEnumerator OnTerminate()
     {
         col.enabled = false;
-        yield return new WaitForSeconds(1.5f); // 파괴 애니메이션 길이만큼 딜레이
+        yield return new WaitForSeconds(1.25f); // 파괴 애니메이션 길이만큼 딜레이
         this.gameObject.SetActive(false);
         Debug.Log("파괴");
     }
