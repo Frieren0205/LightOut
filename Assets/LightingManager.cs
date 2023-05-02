adb1e7935f7bc518da4e451fd12a64a1fc12aac2
RiHen <31790618+Frieren0205@users.noreply.github.com>
2023-05-03 00:36:43 +0900

    베이크 성공![2023_05_03]

--- Assets/LightingManager.cs
+++ Assets/LightingManager.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 public class LightingManager : MonoBehaviour
 {
 
-    public Texture2D lightmap1_A1;
-    public Texture2D lightmap1_A2;
-    public Texture2D lightmap2_A1;
-    public Texture2D lightmap2_A2;
-    public LightmapData[] lightmapDatas = new LightmapData[5];
-
+    public LightProbes test;
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
