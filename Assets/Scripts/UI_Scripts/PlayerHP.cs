8974cdcb3c7c0287c8693b16ff61f42f0f332908
Frieren0205 <31790618+Frieren0205@users.noreply.github.com>
2023-09-07 19:37:37 +0900

    _

--- Assets/Scripts/UI_Scripts/PlayerHP.cs
+++ Assets/Scripts/UI_Scripts/PlayerHP.cs
@@ -17,14 +17,6 @@ public class PlayerHP : MonoBehaviour
     public Volume volume;
     public VolumeProfile isNormalVolume;
     public VolumeProfile isDangerousVolume;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         OnValueChanged();
